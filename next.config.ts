bf0ba630078dbc6f82db8faafee93321fc1462cd
Ashutosh Chaturvedi <ashutoshchat605@gmail.com>
2026-08-29 10:52:11 +0530

    Authentication Done

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  serverExternalPackages: [],
+  allowedDevOrigins: ['10.163.108.224', 'localhost'],
 };
 
 export default nextConfig;
